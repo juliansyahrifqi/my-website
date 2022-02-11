1dc0dfb657cd45163b3bd0426103f754f3586116
Rifqi Pratama <rifqipratama9@gmail.com>
2022-02-11 22:24:18 +0700

    add cursor pointer to card

--- src/components/BlogFeatured.js
+++ src/components/BlogFeatured.js
@@ -4,7 +4,7 @@ import userPhoto from '../assets/user.jpg';
 
 function BlogFeatured({ category, date, title, description, imageSrc  }) {
   return (
-    <div className="box-border card w-full p-6 rounded-3xl hover:bg-hover-card transition ease-in-out hover:outline outline-2 outline-hover-border duration-150">
+    <div className="card w-full p-6 rounded-3xl hover:bg-hover-card transition ease-in-out hover:outline outline-2 outline-hover-border duration-150 cursor-pointer">
       <div className="md:flex items-stretch">
         <img src={imageSrc} className="md:w-1/2 rounded-3xl" />
 
