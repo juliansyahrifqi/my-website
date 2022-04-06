50339cc19cd0defeaa46a21a450917b36e159920
Rifqi Pratama <rifqipratama9@gmail.com>
2022-04-07 06:23:15 +0700

    change on blog post and featured blog component

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -4,20 +4,20 @@ import { formatDate } from '../utils/utils';
 
 function BlogPost({ blog }) {
     return (
-      <div className="card p-6 rounded-3xl transition duration-150 ease-in hover:bg-[#1F2B42] hover:outline outline-2 outline-[#4A6597] duration-150 cursor-pointer">
-        <img src={blog.coverImage} className="w-full rounded-3xl" />
+      <div className="card p-6 rounded-xl transition duration-150 ease-in hover:bg-[#1F2B42] hover:outline outline-2 outline-[#4A6597] duration-150 cursor-pointer">
+        <img src={blog.coverImage} className="w-full rounded-2xl" />
 
-        <p className="mt-3 text-sm font-normal" style={{ color: "#B8BCC2" }}>
+        <p className="mt-3 text-xs font-normal" style={{ color: "#B8BCC2" }}>
           {formatDate(blog.dateAdded)}
         </p>
 
-        <h1 className="font-bold mt-4 text-xl text-white">{blog.title}</h1>
+        <h1 className="font-bold mt-4 text-md text-white">{blog.title}</h1>
 
         <p
-          className="text-sm mt-3 text-base font-normal"
+          className="text-sm mt-3 font-normal md:leading-6 md:tracking-wide"
           style={{ color: "#B8BCC2" }}
         >
-          {blog.brief.substring(0, 150)}...
+          {blog.brief.substring(0, 80)}...
         </p>
       </div>
     );
--- src/components/BlogPostFeatured.js
+++ src/components/BlogPostFeatured.js
@@ -4,16 +4,16 @@ import { formatDate } from '../utils/utils';
 
 function BlogPostFeatured({ blog }) {
   return (
-    <div className="card w-full p-6 rounded-3xl hover:bg-[#1F2B42]  transition ease-in-out hover:outline outline-2 outline-[#4A6597] duration-150 cursor-pointer">
+    <div className="card w-full p-6 rounded-xl md:rounded-3xl hover:bg-[#1F2B42] transition ease-in-out hover:outline outline-2 outline-[#4A6597] duration-150 cursor-pointer">
       <div className="md:flex md:items-center">
         <img src={blog.coverImage} className="md:w-1/2 rounded-3xl" />
 
         <div className="information md:mx-5">
-          <p className="mt-5 text-sm md:text-base" style={{ color: "#B8BCC2" }}>
+          <p className="mt-2 text-sm" style={{ color: "#B8BCC2" }}>
             {formatDate(blog.dateAdded)}
           </p>
 
-          <h1 className="text-2xl md:text-4xl font-bold text-white md:leading-normal mt-5 ">
+          <h1 className="text-2xl md:text-2xl font-bold text-white md:leading-normal mt-4 ">
             {blog.title}
           </h1>
 
@@ -21,7 +21,7 @@ function BlogPostFeatured({ blog }) {
             className="text-sm md:text-base font-normal mt-4 md:mt-4 md:leading-7 md:tracking-wide"
             style={{ color: "#B8BCC2" }}
           >
-            {blog.brief}
+            {blog.brief?.substring(0, 150)}...
           </p>
         </div>
       </div>
